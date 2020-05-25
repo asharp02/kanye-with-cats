7038f8c316243eaf55aa234ca2ccb51ffb209c0b
Adrian Sharpe <asharpe@nationaljournal.com>
2020-05-25 12:22:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let quotesDiv = document.querySelector("#quotes");
+
+fetch("https://api.kanye.rest")
+    .then(res => res.json())
+    .then(quote => {
+        console.log(quote);
+        quotesDiv.innerHTML += `<p>${quote.quote}</p>`;
+    });
+
+let catButton = document.querySelector("#give-cat");
+let catDiv = document.querySelector("#cat-pic");
+catButton.addEventListener("click", evt => {
+    fetch("https://api.thecatapi.com/v1/images/search")
+        .then(res => res.json())
+        .then(kitty => {
+            console.log(kitty);
+            catDiv.innerHTML += `
+            <p> Here is a nice kitty </p>
+            <img src=${kitty[0].url} height="250" width="250">`;
+        });
+});
